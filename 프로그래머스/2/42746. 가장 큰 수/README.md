9646da198693a762aa6593031224f192d6ba72fe
JoeGukseon <116003804+JoeGukseon@users.noreply.github.com>
2024-01-26 09:12:40 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.36 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=c) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.03 ms
+메모리: 4.36 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 9:5:14
+2024년 1월 5일 9:12:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare(const void *a, const void *b) {
+    char str1[20], str2[20];
+    sprintf(str1, "%d%d", *(int*)b, *(int*)a);
+    sprintf(str2, "%d%d", *(int*)a, *(int*)b);
+    return strcmp(str1, str2);
+}
+
+// numbers_len은 배열 numbers의 길이입니다.
+char* solution(int numbers[], size_t numbers_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int isAllZero = 1;
+    for (int i = 0; i < numbers_len; ++i) {
+        if (numbers[i] != 0) {
+            isAllZero = 0;
+            break;
+        }
+    }
+    
+    if (isAllZero) {
+        return "0";
+    }
+    
+    qsort(numbers, numbers_len, sizeof(int), compare);
+    
+    char* answer = (char*)malloc(20 * numbers_len);
+    answer[0] = '\0';
+
+    for (size_t i = 0; i < numbers_len; ++i) {
+        char numStr[20];
+        sprintf(numStr, "%d", numbers[i]);
+        strcat(answer, numStr);
+    }
+    
+    return answer;
+}
